a410bc2ab35d711e67351a910dbf92424e161cbb
lkrauter <lkrauter@stone-e.de>
2020-03-04 15:09:53 +0100

    c# Console Application SetUp

---
+++ SOURCE/MOD_030_ConsoleApplication/ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
