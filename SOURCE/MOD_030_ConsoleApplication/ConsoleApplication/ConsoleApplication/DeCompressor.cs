26bc267ca260571f4e309a2e2452b7e5b033ae96
Luc <49071530+LucK1Y@users.noreply.github.com>
2020-03-16 09:43:48 +0100

    Update DeCompressor.cs
    Decompress function

--- SOURCE/MOD_030_ConsoleApplication/ConsoleApplication/ConsoleApplication/DeCompressor.cs
+++ SOURCE/MOD_030_ConsoleApplication/ConsoleApplication/ConsoleApplication/DeCompressor.cs
@@ -12,7 +12,8 @@ namespace ConsoleApplication
 
         public void DeCompress(string file_path_src,string folder_path_target)
         {
-
+            var container = new Aml.Engine.AmlObjects.AutomationMLContainer(file_path_src);
+            container.ExtractAllFiles(folder_path_target);
         }
     }
 }
