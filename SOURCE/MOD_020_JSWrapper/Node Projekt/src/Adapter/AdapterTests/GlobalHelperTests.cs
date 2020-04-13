7786a171461a2ebab7df6ff8f16b4a6ee230eb93
Privat <mail>
2020-04-13 11:12:35 +0200

    Unit tests added

---
+++ SOURCE/MOD_020_JSWrapper/Node Projekt/src/Adapter/AdapterTests/GlobalHelperTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Adapter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter.Tests
+{
+    [TestClass()]
+    public class GlobalHelperTests
+    {
+        [TestMethod()]
+        public void dynamicPayloadHasKeysTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ SOURCE/MOD_020_JSWrapper/Node Projekt/src/Adapter/Adapter_unittestsuit/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Adapter_unittestsuit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
